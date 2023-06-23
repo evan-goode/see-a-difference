64929a650d750722887f15b773d01cf29861b759
Evan Goode <mail@evangoo.de>
2023-06-22 20:45:23 -0400

    more color schemes

--- source/constants.js
+++ source/constants.js
@@ -10,15 +10,35 @@ export const CSV_FILENAME = "sexdifference.csv";
 export const HATCH = false;
 
 export const COLOR_SCHEMES = {
+	"Red & Blue": {
+		a: "#82ace5",
+		b: "#af3742",
+		intersect: "#9666b8",
+	},
 	"Pink & Blue": {
 		a: "#94c9fd",
 		b: "#fe91c9",
 		intersect: "#8e8ce4",
 	},
-	"Red & Blue": {
-		a: "#82ace5",
-		b: "#af3742",
-		intersect: "#9666b8",
+	"Brown & Yellow": {
+		a: "#ffbf69",
+		b: "#7c3626",
+		intersect: "#be7b48",
+	},
+	"Poppy & Sunglow": {
+		a: "#fdca40",
+		b: "#df2935",
+		intersect: "#ee7a3b",
+	},
+	"Teal & Pumpkin": {
+		a: "#f5853f",
+		b: "#368f8b",
+		intersect: "#7c3626",
+	},
+	"Brown & Gray": {
+		a: "#d3d2c7",
+		b: "#4c2719",
+		intersect: "#907d70",
 	},
 };
 export const DEFAULT_COLOR_SCHEME = "Red & Blue";
